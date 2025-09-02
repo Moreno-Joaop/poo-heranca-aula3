27c3b60c55a8c629acec264f9cbf92674adc656e
Moreno-Joaop <joaopedromorenoalegre@gmail.com>
2025-09-02 01:26:42 +0000

    Aula 01-09-2025-V2

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -3,10 +3,17 @@ package app;
 public class Main {
     public static void main(String[] args){
         Tarefa[] tarefas = new Tarefa[5];
+        Compromisso[] compromissos = new Compromisso[5];
+
 
         tarefas[0] = new Tarefa(0, "Teste", false);
         tarefas[4] = new Tarefa(4, "novo teste", false);
-            
+
+        compromissos[2] = new Compromisso(0, "Compromisso", "hoje", "amanhã");
+
+
         System.out.println(tarefas[0].getDescricao());
+        System.out.println(tarefas[4].getDescricao());
+        System.out.println(compromissos[2].getDescricao());
     }
 }

b0b345c50ed1c461e8cf37179ac1e013e150cf7d
Moreno-Joaop <joaopedromorenoalegre@gmail.com>
2025-09-09 00:41:37 +0000

    Aula08-09-2025-semHerança

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,19 +1,29 @@
 package app;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
     public static void main(String[] args){
-        Tarefa[] tarefas = new Tarefa[5];
-        Compromisso[] compromissos = new Compromisso[5];
+        // Tarefa[] tarefas = new Tarefa[5];
+        // Compromisso[] compromissos = new Compromisso[5];
 
 
-        tarefas[0] = new Tarefa(0, "Teste", false);
-        tarefas[4] = new Tarefa(4, "novo teste", false);
+        // tarefas[0] = new Tarefa(0, "Teste", false);
+        // tarefas[4] = new Tarefa(4, "novo teste", false);
 
-        compromissos[2] = new Compromisso(0, "Compromisso", "hoje", "amanhã");
+        // compromissos[2] = new Compromisso(0, "Compromisso", "hoje", "amanhã");
 
 
-        System.out.println(tarefas[0].getDescricao());
-        System.out.println(tarefas[4].getDescricao());
-        System.out.println(compromissos[2].getDescricao());
+        // System.out.println(tarefas[0].getDescricao());
+        // System.out.println(tarefas[4].getDescricao());
+        // System.out.println(compromissos[2].getDescricao());
+
+    PessoaFisica pf = new PessoaFisica("João", "R. Uapes, 215", LocalDate.now() , LocalDate.of(2007, Month.FEBRUARY, 26), "441.922.638-23");
+
+    PessoaJuridica pj = new PessoaJuridica("JPMA Repr", "Rua dos Bobos, 0", LocalDate.now(),"53.009.0001.52", "JPMA Lindão");
+
+    System.out.println(pf.exibirInformacoes());
+    System.out.println(pj.exibirInformacoes());
     }
 }
